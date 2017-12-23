0a52253a5adde7a8fee2e0b16ff29c8188fbf7fe
Emmanouil Konstantinidis <hello@manos.im>
2017-12-23 01:04:20 +0000

    Now with SSL

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $("#latest-version").hide();
+  $(".latest-version").hide();
 
   $('.scroll-to-top').click(function() {
     $('html, body').animate({scrollTop : 0},800);
